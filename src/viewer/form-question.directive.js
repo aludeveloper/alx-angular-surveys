7e837ae092d7021c443fc5bc9480f9c8f6aed13b
Ankit <ankit.mindnerves@gmail.com>
2018-07-03 12:45:34 +0530

    multilinked questions

--- src/viewer/form-question.directive.js
+++ src/viewer/form-question.directive.js
@@ -100,7 +100,6 @@ angular.module('mwFormViewer').factory("FormQuestionId", function() {
                     ctrl.isAnswerSelected = false;
                     ctrl.initialized = true;
                 };
-
                 
                 ctrl.hideLinked = function(qdata){
                     $timeout(function() {
@@ -108,41 +107,40 @@ angular.module('mwFormViewer').factory("FormQuestionId", function() {
                         console.log("qdata",qdata);
 
                         if ($rootScope.linkedquestionList.includes(qdata.id)) {
-                            // ctrl.question['isLinked'] = true;
                             document.getElementById(qdata.id).style.display = "none";
                         }
-                        /*else{
-                            ctrl.question['isLinked'] = false;
-                        }*/
-
-
 
                         if (qdata.type == "radio") {
                             angular.forEach(qdata.offeredAnswers, function(offans, key1) {
-                                if(!$rootScope.linkedquestionList.includes(offans.linkedquestion)){
-                                    $rootScope.linkedquestionList.push(offans.linkedquestion);
+                                for(var i=0; i<offans.linkedquestion.length; i++){
+                                    if(!$rootScope.linkedquestionList.includes(offans.linkedquestion[i])){
+                                        $rootScope.linkedquestionList.push(offans.linkedquestion[i]);
+                                    }                                    
                                 }
                             });                         
                         }
-                    }, 300);
-                    
+                    }, 300);                    
+                };
+
+                ctrl.dateChanged = function(date){
+                    ctrl.questionResponse.answer = date ? moment(date).startOf('day').format('DD-MM-YYYY') : '';                    
                 };
 
                 ctrl.selectedAnswerChanged = function() {
                     if(ctrl.selectedLinkQ === undefined){
                         ctrl.selectedLinkQ = ctrl.questionResponse.selectedAnswer.linkedquestion;
-                        document.getElementById(ctrl.selectedLinkQ).style.display = "block";
+                        for (var i = 0; i < ctrl.selectedLinkQ.length; i++) {
+                            document.getElementById(ctrl.selectedLinkQ[i]).style.display = "block";
+                        }
                     }else{
-                        document.getElementById(ctrl.selectedLinkQ).style.display = "none";
+                        for (var i = 0; i < ctrl.selectedLinkQ.length; i++) {
+                            document.getElementById(ctrl.selectedLinkQ[i]).style.display = "none";
+                        }
                         ctrl.selectedLinkQ = ctrl.questionResponse.selectedAnswer.linkedquestion;
-                        document.getElementById(ctrl.selectedLinkQ).style.display = "block";
+                        for (var i = 0; i < ctrl.selectedLinkQ.length; i++) {
+                            document.getElementById(ctrl.selectedLinkQ[i]).style.display = "block";
+                        }
                     }
-                    
-                    // console.log("ctrl",ctrl);
-                    console.log("ctrl.questionResponse.selectedAnswer",ctrl.questionResponse.selectedAnswer);
-                    // console.log(ctrl.question.isLinked);
-                    // console.log(ctrl.questionResponse.selectedAnswer.linkedquestion);
-                    console.log(ctrl.selectedLinkQ);
                     
                     delete ctrl.questionResponse.other;
                     ctrl.isOtherAnswer = false;

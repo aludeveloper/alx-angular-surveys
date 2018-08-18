2032a718156b1fd9c7b6163f75378500a471b8f2
mindnervestech <ankit.mindnerves@gmail.com>
2018-08-18 19:20:06 +0530

    single element width fix

--- src/viewer/form-viewer.directive.js
+++ src/viewer/form-viewer.directive.js
@@ -137,31 +137,62 @@ angular.module('mwFormViewer').directive('mwFormViewer', ["$rootScope", function
 						$timeout(ctrl.resetPages, 0);
 					}
 				}
-        
+
+				console.log("ctrl.currentPage",ctrl.currentPage);
 				$timeout(function() {
-					var temp = [];					
+					var arr = [];
 					angular.forEach(ctrl.currentPage.elements,function(item,index) {	          	 	
-	                 	temp.push(item.rowNumber);
+	                 	arr.push(item.rowNumber);
 	          	});
 
-	          	for(var i=0;i<temp.length;i++){
-	          		if(i==0){
-	          			ctrl.singleElRow.push(temp[i]);
-	          		}else{
-	          			if(ctrl.singleElRow.includes(temp[i])){
-	          				ctrl.singleElRow.pop();
-	          			}else{
-	          				ctrl.singleElRow.push(temp[i]);
-	          			}
-	          		}
-	          	};	          	
+					// var len = temp.length;
+					// for(var i=0; i<len; i++){
+					// 	for(var j=i+1; j<len; j++){
+					// 		if(temp[i] == temp[j]){
+					// 			temp2.push(temp[i]);
+					// 			temp.splice(j,1);
+					// 			j--;
+					// 			len--;
+					// 		}
+					// 	}
+					// }
+
+					//var arr = [9, 9, 111, 2, 3, 4, 4, 5, 7];
+					var sorted_arr = arr.sort();
+					var results = [];
+					for (var i = 0; i < arr.length - 1; i++) {
+					    if (sorted_arr[i + 1] == sorted_arr[i]) {
+					        results.push(sorted_arr[i]);
+					    }
+					}
+					//var t = [];
+					for(var i in arr){
+						if(!results.includes(arr[i])){
+							ctrl.singleElRow.push(arr[i]);
+					    }
+					}
+					//ctrl.singleElRow = t;
+
+	          	// for(var i=0;i<temp.length;i++){
+	          	// 	if(ctrl.singleElRow.length == 0){
+	          	// 		ctrl.singleElRow.push(temp[i]);
+	          	// 	}else{
+	          	// 		if(ctrl.singleElRow.includes(temp[i])){
+	          	// 			ctrl.singleElRow.pop();
+	          	// 		}else{
+	          	// 			ctrl.singleElRow.push(temp[i]);
+	          	// 		}
+	          	// 	}
+	          	// };
+	          	console.log("ctrl.singleElRow",ctrl.singleElRow);
 				}, 3000);
 				
 			};
 
 			ctrl.singleRow = function(index){
 				//console.log("ctrl.singleElRow.includes(index)",ctrl.singleElRow.includes(index));
-				//return ctrl.singleElRow.includes(index+1);
+				console.log("singleRow",index,ctrl.currentPage.elements);
+				return ctrl.singleElRow.includes(index);
 			};
 
 			//returning paragraph as html
@@ -275,9 +306,15 @@ angular.module('mwFormViewer').directive('mwFormViewer', ["$rootScope", function
 					});
 				});*/
 
+				console.log("ctrl.formData.pages",ctrl.formData.pages);
+
 				for(var i=0; i<ctrl.formData.pages.length; i++){
 					ctrl.formData.pages[i].elements=$filter('orderBy')(ctrl.formData.pages[i].elements, 'rowNumber');
 				}
+
+				console.log("SORTED ctrl.formData.pages",ctrl.formData.pages);
+				// ctrl.currentPage  = $filter('orderBy')(ctrl.currentPage.elements, 'rowNumber');
+				// console.log("sorted ctrl.currentPage",ctrl.currentPage);
 				 ctrl.totalPageLength = ctrl.formData.pages.length;
 
 

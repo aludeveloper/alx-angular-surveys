fd92d4fd973886cc05bf21add3ea4b200c2e2c78
mindnervestech <ankit.mindnerves@gmail.com>
2018-08-27 15:14:02 +0530

    row validations

--- src/builder/form-builder.directive.js
+++ src/builder/form-builder.directive.js
@@ -39,7 +39,7 @@ angular.module('mwFormBuilder').directive('mwFormBuilder', ["$rootScope", functi
                         ctrl.formData.pages.length=0;
                         ctrl.formData.pages.push(createEmptyPage(1));
                         $rootScope.defaultRowNumber=0;
-                    }
+                    };
                 }
             };
             
@@ -53,6 +53,7 @@ angular.module('mwFormBuilder').directive('mwFormBuilder', ["$rootScope", functi
             ctrl.addPage = function(){
                 ctrl.formData.pages.push(createEmptyPage(ctrl.formData.pages.length+1));
                 ctrl.lastPage();
+                $rootScope.defaultRowNumber=0;
                 $rootScope.$broadcast("mwForm.pageEvents.pageAdded");
             };
             ctrl.onChangePageSize = function(){
@@ -124,6 +125,7 @@ angular.module('mwFormBuilder').directive('mwFormBuilder', ["$rootScope", functi
             ctrl.removePage=function(page){
                 var index = ctrl.formData.pages.indexOf(page);
                 ctrl.formData.pages.splice(index,1);
+                $rootScope.defaultRowNumber=0;
                 updatePageNumbers();
                 $rootScope.$broadcast("mwForm.pageEvents.pageRemoved");
                 ctrl.onChangePageSize();
@@ -197,7 +199,6 @@ angular.module('mwFormBuilder').directive('mwFormBuilder', ["$rootScope", functi
                                     }
                                 });
                             }
-
                         });
                     });
                 });
